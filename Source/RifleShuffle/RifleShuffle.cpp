40c47a3d2e3c867ec995452ba5278a1161950c5f
F1ZZ <TheKubonn@hotmail.com>
2024-02-01 00:12:15 +0100

    Initial push

---
+++ Source/RifleShuffle/RifleShuffle.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RifleShuffle.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RifleShuffle, "RifleShuffle" );
